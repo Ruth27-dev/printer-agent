855b582748643de50366adad4b7f74ea2ad7dcda
Ruth <sopharuth.club@gmail.com>
2026-01-01 23:55:15 +0700

    reduce size build

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  productionBrowserSourceMaps: process.env.NEXT_DISABLE_SOURCEMAPS ? false : undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
